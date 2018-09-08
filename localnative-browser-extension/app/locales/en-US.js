59a7da4d6fdf5bf46194f36e2847cfd8d53037c6
Yi Wang <e@yi-wang.me>
2018-09-08 13:49:40 -0400

    feat: i18n works

---
+++ localnative-browser-extension/app/locales/en-US.js
@@ -0,0 +1,14 @@
+﻿locales['en-US'] = i18n.create({
+  values: {
+    "ssbify": 'ssbify',
+    "public": 'public',
+    "language": 'language',
+    "description": "description",
+    "title": 'title',
+    "url": 'url',
+    "type to add tags, enter to save, comma or space as tag seperator": 'type to add tags, enter to save, comma or space as tag seperator',
+    "type to search": 'type to search',
+    "clear search term(s)": 'clear search term(s)'
+  }
+});
+
