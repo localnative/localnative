cc41956d3be724d2977872461db5116f0dd1c797
Yi Wang <e@yi-wang.me>
2018-10-03 18:45:54 -0400

    feat: add localnative-ios

---
+++ localnative-ios/ln-ios/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ln-ios
+//
+//  Created by Yi Wang on 9/16/18.
+//  Copyright © 2018 Yi Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
