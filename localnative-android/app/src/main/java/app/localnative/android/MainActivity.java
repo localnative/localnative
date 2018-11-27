6caa926fa3a1a37b306b27077fe7108141032911
Yi Wang <e@yi-wang.me>
2018-11-26 20:03:15 -0500

    feat: android SearchView

--- localnative-android/app/src/main/java/app/localnative/android/MainActivity.java
+++ localnative-android/app/src/main/java/app/localnative/android/MainActivity.java
@@ -1,26 +1,83 @@
 package app.localnative.android;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.TextView;
 
 import app.ln.R;
-import app.localnative.android.RustBridge;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements SearchView.OnQueryTextListener{
     static {
         System.loadLibrary("localnative_core");
     }
 
     @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Get the SearchView and set the searchable configuration
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        getMenuInflater().inflate(R.menu.toolbar, menu);
+        MenuItem searchItem = menu.findItem(R.id.toolbar_search);
+        SearchView searchView =
+                (SearchView) searchItem.getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+        if (searchView != null) {
+            searchView.setOnQueryTextListener(this);
+        }
+        return super.onCreateOptionsMenu(menu);
+    }
+
+
+    @Override
+    public boolean onQueryTextSubmit(String query) {
+        Log.d("onQueryTextSubmit",  query);
+        return false;
+    }
+
+    @Override
+    public boolean onQueryTextChange(String query) {
+        Log.d("onQueryTextChange", query);
+        return false;
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(true);
+
+        Intent intent = getIntent();
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            String query = intent.getStringExtra(SearchManager.QUERY);
+            doSearch(query);
+        }
 
         RustBridge r = new RustBridge();
         //TODO detect allow write to storage permission
         String s = r.run("{\"action\": \"select\", \"limit\":10,\"offset\":0}");
         ((TextView)findViewById(R.id.searchText)).setText(s);
+    }
+
+    private void doSearch(String query) {
+        Log.d("doSearch", query);
     }
 
 }
