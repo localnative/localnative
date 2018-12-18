6afc472117ead5c4a241321f796f33f85839131e
Yi Wang <e@yi-wang.me>
2018-12-17 22:20:35 -0500

    feat: get title from url response

--- localnative-android/app/src/main/java/app/localnative/android/ShareActivity.kt
+++ localnative-android/app/src/main/java/app/localnative/android/ShareActivity.kt
@@ -5,13 +5,21 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import app.localnative.R
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import kotlinx.android.synthetic.main.activity_share.*
 import org.json.JSONObject
+import android.text.method.ScrollingMovementMethod
+import java.net.URLDecoder
 
 class ShareActivity : AppCompatActivity(), Permission.OnPermissonGrantedListenr {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_share)
+        tagsText.requestFocus()
+        textView.movementMethod = ScrollingMovementMethod()
         btnCancel.setOnClickListener {
             finish()
         }
@@ -58,6 +66,24 @@ class ShareActivity : AppCompatActivity(), Permission.OnPermissonGrantedListenr 
     private fun handleSendText(intent: Intent) {
         intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
             urlText.setText(it)
+
+            val queue = Volley.newRequestQueue(this)
+            val url = it
+
+            val stringRequest = StringRequest(Request.Method.GET, url,
+                    Response.Listener<String> { response ->
+                        val r =  response.trim()
+                        textView.text = r.substring(0, minOf(50000, r.length))
+                        val re = Regex("""<(?i)(Title)>(.*?)<\\?/(?i)(title)>""")
+                        re.find(r)?.let{
+                            val (_, t, _)=it.destructured
+                            val title =  t.trim()
+                            titleText.setText(title.substring(0, minOf(500,title.length)))
+                        }
+                    },
+                    Response.ErrorListener { textView.text = "url response error!" })
+
+            queue.add(stringRequest)
         }
     }
 }
