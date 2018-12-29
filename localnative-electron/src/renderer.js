fd3e44abccfd95eb5605181d1c5952059ea2981c
Yi Wang <e@yi-wang.me>
2018-12-29 17:51:44 -0500

    feat: open link in os browser

--- localnative-electron/src/renderer.js
+++ localnative-electron/src/renderer.js
@@ -137,6 +137,15 @@ document.addEventListener('DOMContentLoaded', function () {
   // initial query
   cmdSelect();
 
+  // Open all links in external browser
+  let shell = require('electron').shell
+  document.addEventListener('click', function (event) {
+    if (event.target.tagName === 'A' && event.target.href.startsWith('http')) {
+      event.preventDefault()
+      shell.openExternal(event.target.href)
+    }
+  })
+
 });
 
 let LIMIT = 10;
