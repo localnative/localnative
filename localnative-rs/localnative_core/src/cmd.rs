728f705deade0d802f8e2f80fe240ddf5f082369
Yi Wang <e@yi-wang.me>
2019-01-13 17:15:58 -0500

    feat: multiple word text search

--- localnative-rs/localnative_core/src/cmd.rs
+++ localnative-rs/localnative_core/src/cmd.rs
@@ -19,34 +19,73 @@ pub fn count(conn: &Connection, tbl: &str) -> i64 {
 }
 
 pub fn search(conn: &Connection, query: &str, limit: &u32, offset: &u32) -> String {
-    let mut stmt = conn.prepare(
+    let re1 = Regex::new(r"\s+").unwrap();
+    let s1 = re1.replace_all(query, " ");
+    let words = s1
+        .trim()
+        .split(" ")
+        .map(|w| format!("%{}%", w))
+        .collect::<Vec<String>>();
+    if words.len() == 1 && words.get(0).unwrap().is_empty() {
+        return select(conn, limit, offset);
+    }
+    let num_words = words.len();
+    eprintln!("{} words {:?}", num_words, words);
+
+    let r: Vec<String> = (0..num_words)
+        .map(|i| {
+            format!(
+                "(
+        title like :w{}
+        or url like :w{}
+        or tags like :w{}
+        or description like :w{}
+        )",
+                i.to_string(),
+                i.to_string(),
+                i.to_string(),
+                i.to_string()
+            )
+        })
+        .collect();
+
+    let sql = format!(
         "SELECT rowid, title, url, tags, description, comments, annotations, created_at, is_public
-        FROM note
-        where title like :query
-        or url like :query
-        or tags like :query
-        or description like :query
-        order by created_at desc limit :limit offset :offset"
-        ).unwrap();
+        FROM note where
+        {}
+        order by created_at desc limit :limit offset :offset",
+        r.join(" and ")
+    );
+
+    eprintln!("sql {}", sql);
+
+    let mut stmt = conn.prepare(&sql).unwrap();
+
+    let keys: Vec<String> = (0..num_words)
+        .map(|i| ":w".to_string() + &i.to_string())
+        .collect();
+
+    let mut params: Vec<(&str, &ToSql)> =
+        vec![(":limit", limit as &ToSql), (":offset", offset as &ToSql)];
+
+    for i in 0..num_words {
+        params.push((&keys.get(i).unwrap(), words.get(i).unwrap() as &ToSql));
+    }
+
+    eprintln!("params {:?}", params.len());
+
     let note_iter = stmt
-        .query_map_named(
-            &[
-                (":query", &format!("%{}%", query)),
-                (":limit", limit as &ToSql),
-                (":offset", offset as &ToSql),
-            ],
-            |row| Note {
-                rowid: row.get(0),
-                title: row.get(1),
-                url: row.get(2),
-                tags: row.get(3),
-                description: row.get(4),
-                comments: row.get(5),
-                annotations: "".to_string(), //row.get(6),
-                created_at: row.get(7),
-                is_public: row.get(8),
-            },
-        )
+        .query_map_named(&params, |row| Note {
+            rowid: row.get(0),
+            title: row.get(1),
+            url: row.get(2),
+            tags: row.get(3),
+            description: row.get(4),
+            comments: row.get(5),
+            annotations: "".to_string(), //row.get(6),
+            created_at: row.get(7),
+            is_public: row.get(8),
+        })
         .unwrap();
 
     let mut j = "[ ".to_owned();
