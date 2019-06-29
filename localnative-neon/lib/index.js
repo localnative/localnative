e6a8af651f2bab0b8dd972e624c4c635b435f886
Yi Wang <e@yi-wang.me>
2019-06-29 17:47:28 -0400

    chore: set-version 0.4.0

--- localnative-neon/lib/index.js
+++ localnative-neon/lib/index.js
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU Affero General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
-let version = '0.3.10'
+let version = '0.4.0'
 
 var platform = "-gnu-linux"
 switch(process.platform){
