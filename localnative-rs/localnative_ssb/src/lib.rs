82aebccbf48af2edf799fa8c19bc3bb59fb9b4f0
Yi Wang <e@yi-wang.me>
2019-08-25 22:32:29 -0400

    chore: set-version 0.4.1

--- localnative-rs/localnative_ssb/src/lib.rs
+++ localnative-rs/localnative_ssb/src/lib.rs
@@ -30,7 +30,7 @@ fn get_ssb_cli_path() -> String {
     format!(
         "{}/LocalNative/bin/localnative-nodejs-{}",
         dirs::home_dir().unwrap().to_str().unwrap(),
-        "0.4.0"
+        "0.4.1"
     )
 }
 
