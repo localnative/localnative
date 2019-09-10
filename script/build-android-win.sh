c612448021c488883f9d057f5adf5759ba5d56a5
Yi Wang <e@yi-wang.me>
2019-09-09 21:26:42 -0400

    feat: add build-android-mac.sh etc

---
+++ script/build-android-win.sh
@@ -0,0 +1,7 @@
+BIN_PATH=$HOME/Library/Android/sdk/ndk-bundle/toolchains/llvm/prebuilt/darwin-x86_64/bin
+cd `dirname $0`/../localnative-rs/localnative_core
+# cargo clean
+PATH=$BIN_PATH:$PATH cargo build --target armv7-linux-androideabi --release
+PATH=$BIN_PATH:$PATH cargo build --target i686-linux-android --release
+PATH=$BIN_PATH:$PATH cargo build --target aarch64-linux-android --release
+PATH=$BIN_PATH:$PATH cargo build --target x86_64-linux-android --release
