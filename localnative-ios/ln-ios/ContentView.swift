823a50fba3775bbae4ee12eab51b69e1af9fdeb5
Yi Wang <e@yi-wang.me>
2020-04-12 21:49:07 -0400

    feat: SwiftUI works

---
+++ localnative-ios/ln-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  localnative-ios
+//
+//  Created by Yi Wang on 4/12/20.
+//  Copyright © 2020 Yi Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
