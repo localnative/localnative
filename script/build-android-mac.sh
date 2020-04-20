0a368002e74276406dbe702d96e31b66328e4320
Yi Wang <e@yi-wang.me>
2020-04-19 20:40:41 -0400

    fix: build-android-mac.sh

--- script/build-android-mac.sh
+++ script/build-android-mac.sh
@@ -1,4 +1,4 @@
-BIN_PATH=$HOME/Library/Android/sdk/ndk-bundle/toolchains/llvm/prebuilt/darwin-x86_64/bin
+BIN_PATH=$HOME/Library/Android/sdk/ndk/21.0.6113669/toolchains/llvm/prebuilt/darwin-x86_64/bin
 cd `dirname $0`/../localnative-rs/localnative_core
 # cargo clean
 PATH=$BIN_PATH:$PATH cargo build --target armv7-linux-androideabi --release
