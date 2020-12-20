45948333e5fc6697fe3281f1b03695d0def048d3
Yi Wang <e@yi-wang.me>
2020-12-19 19:37:36 -0500

    fix: update build-android-gnu-linux.sh

--- script/build-android-gnu-linux.sh
+++ script/build-android-gnu-linux.sh
@@ -1,6 +1,8 @@
-BIN_PATH=$HOME/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin
+# BIN_PATH=$HOME/Android/Sdk/ndk-bundle/toolchains/llvm/prebuilt/linux-x86_64/bin
+BIN_PATH=$HOME/Android/Sdk/ndk/22.0.7026061/toolchains/llvm/prebuilt/linux-x86_64/bin
+
 cd `dirname $0`/../localnative-rs/localnative_core
-# cargo clean
+#cargo clean
 PATH=$BIN_PATH:$PATH cargo build --target armv7-linux-androideabi --release
 PATH=$BIN_PATH:$PATH cargo build --target i686-linux-android --release
 PATH=$BIN_PATH:$PATH cargo build --target aarch64-linux-android --release
