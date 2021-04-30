5f8b0dd4f2f0742d9899f42cf4fdd20b4873ee45
Yi Wang <e@yi-wang.me>
2021-04-29 22:10:39 -0400

    feat: add build-ios.sh

---
+++ script/build-ios.sh
@@ -0,0 +1,11 @@
+# cargo install cargo-lipo
+
+cd `dirname $0`/../localnative-rs/localnative_core
+
+echo 'crate-type = ["staticlib"]' >> Cargo.toml
+
+# cargo clean
+
+cargo lipo --release
+
+git reset --hard
