f139916d642832a806e384dafdd23eb2078c404f
Yi Wang <e@yi-wang.me>
2021-04-29 21:53:25 -0400

    feat: automate insertion of crate-type for android

--- script/build-android-mac.sh
+++ script/build-android-mac.sh
@@ -1,7 +1,13 @@
 BIN_PATH=$HOME/Library/Android/sdk/ndk-bundle/toolchains/llvm/prebuilt/darwin-x86_64/bin
 cd `dirname $0`/../localnative-rs/localnative_core
+
+echo 'crate-type = ["cdylib"]' >> Cargo.toml
+
 # cargo clean
+
 PATH=$BIN_PATH:$PATH cargo build --target armv7-linux-androideabi --release
 PATH=$BIN_PATH:$PATH cargo build --target i686-linux-android --release
 PATH=$BIN_PATH:$PATH cargo build --target aarch64-linux-android --release
 PATH=$BIN_PATH:$PATH cargo build --target x86_64-linux-android --release
+
+git reset --hard
