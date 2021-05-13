640ace2968823a2fac1b9387ed67f7a9e02e37a3
Extreme <hill.recovery@protonmail.com>
2021-05-13 13:35:21 +0800

    fix: fix add icons and locales to package

--- script/build_linux.sh
+++ script/build_linux.sh
@@ -10,4 +10,6 @@ cp localnative_iced localnative_linux_bin/
 cp localnative-web-ext-host localnative_linux_bin/
 cp ../../LICENSE localnative_linux_bin/
 cp ../../README.md localnative_linux_bin/
+cp -R ../../icons localnative_linux_bin/
+cp -R ../../locales localnative_linux_bin/
 tar -zcvf localnative_linux_bin.tar.gz localnative_linux_bin/
