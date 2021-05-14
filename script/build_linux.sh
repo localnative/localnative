5f73c55436ccf601f573a89887c588213abd81d8
Extreme <hill.recovery@protonmail.com>
2021-05-14 16:19:23 +0800

    fix: remove icons frome package of release

--- script/build_linux.sh
+++ script/build_linux.sh
@@ -10,6 +10,5 @@ cp localnative_iced localnative_linux_bin/
 cp localnative-web-ext-host localnative_linux_bin/
 cp ../../LICENSE localnative_linux_bin/
 cp ../../README.md localnative_linux_bin/
-cp -R ../../icons localnative_linux_bin/
 cp -R ../../locales localnative_linux_bin/
 tar -zcvf localnative_linux_bin.tar.gz localnative_linux_bin/
