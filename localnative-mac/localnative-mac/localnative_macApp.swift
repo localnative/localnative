23a9b654f6335594943ab71d1b92446174d9699d
Yi Wang <yi@yilab.com>
2021-07-10 00:13:27 -0400

    feat: init localnative-mac

---
+++ localnative-mac/localnative-mac/localnative_macApp.swift
@@ -0,0 +1,17 @@
+//
+//  localnative_macApp.swift
+//  localnative-mac
+//
+//  Created by Yi Wang on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct localnative_macApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
