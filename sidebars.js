0b66c28be177a882cb67aa4270a4cc81c9f6af13
Yi Wang <yi@yilab.com>
2021-10-31 17:32:44 -0400

    refactor: move to gui-iced directory

--- sidebars.js
+++ sidebars.js
@@ -13,17 +13,17 @@ module.exports = {
     "Summer of Code": [
         "localnative-2021-soc"
     ],
-    "Rust GUI 中文教程": [
-      "tutorial0",
-      "tutorial1",
-      "tutorial2",
-      "tutorial3",
-      "tutorial4",
-      "tutorial5",
-      "tutorial6",
-      "tutorial7",
-      "tutorial8",
-      "tutorial9",                  
+    "Rust GUI iced 中文教程": [
+      "gui-iced/tutorial0",
+      "gui-iced/tutorial1",
+      "gui-iced/tutorial2",
+      "gui-iced/tutorial3",
+      "gui-iced/tutorial4",
+      "gui-iced/tutorial5",
+      "gui-iced/tutorial6",
+      "gui-iced/tutorial7",
+      "gui-iced/tutorial8",
+      "gui-iced/tutorial9",                  
     ]
   },
 };
