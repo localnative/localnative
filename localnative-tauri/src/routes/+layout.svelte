b4f2e010c396477cc7ae18b6dcc8dee6d76261e0
chenbaiyu <cby40899570@gmail.com>
2022-11-30 20:13:56 +0800

    create tauri project and make basic layout

---
+++ localnative-tauri/src/routes/+layout.svelte
@@ -0,0 +1,67 @@
+<script>
+	import '../app.css';
+	import { page } from '$app/stores';
+	import {
+		faNoteSticky,
+		faCircleExclamation,
+		faBars,
+		faCloudArrowDown
+	} from '@fortawesome/free-solid-svg-icons';
+	import Fa from 'svelte-fa';
+</script>
+
+<div class="flex h-full w-full flex-row ">
+	<div class="h-full flex flex-col z-20">
+		<div>
+			<ul class="menu bg-base-100 p-2">
+				<li class="tooltip tooltip-right" data-tip="便签">
+					<a
+						href="/notes"
+						class="flex justify-center items-center {$page.url.pathname == '/notes'
+							? 'active'
+							: ''}"
+					>
+						<Fa icon={faNoteSticky} size="1.4x" />
+					</a>
+				</li>
+				<li class="tooltip tooltip-right my-1" data-tip="同步">
+					<a
+						href="/sync"
+						class="flex justify-center items-center {$page.url.pathname == '/sync' ? 'active' : ''}"
+					>
+						<Fa icon={faCloudArrowDown} size="1.4x" />
+					</a>
+				</li>
+			</ul>
+		</div>
+		<div class="flex-1" />
+		<div>
+			<ul class="menu bg-base-100 p-2">
+				<li class="tooltip tooltip-right my-1" data-tip="关于">
+					<a
+						href="/about"
+						class="flex justify-center items-center {$page.url.pathname == '/about'
+							? 'active'
+							: ''}"
+					>
+						<Fa icon={faCircleExclamation} size="1.4x" />
+					</a>
+				</li>
+				<li class="tooltip tooltip-right" data-tip="设置">
+					<a
+						href="/settings"
+						class="flex justify-center items-center {$page.url.pathname == '/settings'
+							? 'active'
+							: ''}"
+					>
+						<Fa icon={faBars} size="1.4x" />
+					</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+
+	<div class="h-full flex-1 p-2">
+		<slot />
+	</div>
+</div>
---
+++ localnative-tauri/src/routes/notes/+page.svelte
@@ -0,0 +1,46 @@
+<script lang="ts">
+	import { faMagnifyingGlass, faCalendar } from '@fortawesome/free-solid-svg-icons';
+	import { DatePicker, localeFromDateFnsLocale } from 'date-picker-svelte';
+	import Fa from 'svelte-fa';
+
+	let date = new Date();
+</script>
+
+<div class="w-full h-full flex flex-row gap-2">
+	<div class="flex-1">
+		<div class="form-control">
+			<div class="input-group">
+				<input type="text" placeholder="搜索便签..." class="input input-bordered input-sm w-full" />
+				<div class="dropdown dropdown-end">
+					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+					<!-- svelte-ignore a11y-label-has-associated-control -->
+					<label tabindex="0" class="btn btn-sm rounded-none"><Fa icon={faCalendar} /></label>
+					<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+					<div
+						tabindex="0"
+						class="dropdown-content card card-compact p-2 shadow bg-white text-primary-content"
+					>
+						<div class="card-body flex flex-row">
+							<DatePicker bind:value={date} />
+							<DatePicker bind:value={date} />
+						</div>
+					</div>
+				</div>
+
+				<button class="btn btn-square btn-sm">
+					<Fa icon={faMagnifyingGlass} />
+				</button>
+			</div>
+		</div>
+	</div>
+	<div>
+		<div class="form-control">
+			<div class="input-group">
+				<input type="text" placeholder="搜索标签..." class="input input-bordered input-sm" />
+				<button class="btn btn-square btn-sm">
+					<Fa icon={faMagnifyingGlass} />
+				</button>
+			</div>
+		</div>
+	</div>
+</div>
