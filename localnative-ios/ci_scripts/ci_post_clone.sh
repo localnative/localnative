7f51cbcc944bba942ddc2190efcfc26679c17e7c
ben <benying1988@gmail.com>
2022-12-09 17:34:12 +0800

    fix: script path, refs #78

--- localnative-ios/ci_scripts/ci_post_clone.sh
+++ localnative-ios/ci_scripts/ci_post_clone.sh
@@ -3,7 +3,7 @@
 # Build liblocalnative_core.a
 cargo install cargo-lipo
 rustup target add aarch64-apple-ios x86_64-apple-ios
-../script/build-ios.sh
+../../script/build-ios.sh
 
 # Install CocoaPods using Homebrew.
 brew install cocoapods
