70357a46fc8e3e6a7f6488a1a085e14f5e7f09fd
ben <benying1988@gmail.com>
2022-12-09 17:32:48 +0800

    feat: add build liblocalnative_core.a, refs #78

--- localnative-ios/ci_scripts/ci_post_clone.sh
+++ localnative-ios/ci_scripts/ci_post_clone.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+# Build liblocalnative_core.a
+cargo install cargo-lipo
+rustup target add aarch64-apple-ios x86_64-apple-ios
+../script/build-ios.sh
+
 # Install CocoaPods using Homebrew.
 brew install cocoapods
 
